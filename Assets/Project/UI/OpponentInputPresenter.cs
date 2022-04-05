9d4eb6ee3416706565fa2eeec5468b8803065537
Lee Hall <lee@lee-hall.com>
2022-04-06 00:10:18 +0100

    Added support for getting opponents selection using randomnumberapi webservice

--- Assets/Project/UI/OpponentInputPresenter.cs
+++ Assets/Project/UI/OpponentInputPresenter.cs
@@ -1,4 +1,9 @@
 ﻿using JetBrains.Annotations;
+using Cysharp.Threading.Tasks;
+using UnityEngine.Networking;
+using System;
+using System.Threading;
+using UnityEngine;
 
 namespace Project.UI
 {
@@ -7,13 +12,13 @@ namespace Project.UI
     {        
         private string _selectedSymbol;
 
-        public void SelectSymbol()
+        public async void SelectSymbol()
         {
             string[] symbols = new[] { Symbols.Rock, Symbols.Paper, Symbols.Scissors };
 
-            int selection = GetRandomSelection();
+            int selection = await GetRandomSelection(0, symbols.Length -1);
 
-            _selectedSymbol = symbols[selection - 1];
+            _selectedSymbol = selection >= 0 ? symbols[selection] : "";
         }
 
         public void Reset()
@@ -26,10 +31,28 @@ namespace Project.UI
             return _selectedSymbol;
         }
 
-        private int GetRandomSelection()
+        private async UniTask<int> GetRandomSelection(int min, int max)
         {
-            // TODO: Placeholder logic to be replaced with call to Random Number WebService.
-            return UnityEngine.Random.Range(1, 4);
+            var cts = new CancellationTokenSource();
+            cts.CancelAfterSlim(TimeSpan.FromSeconds(3));
+
+            try
+            {
+                var txt = (await UnityWebRequest.Get($"http://www.randomnumberapi.com/api/v1.0/random?min={min}&max={max}").SendWebRequest()).downloadHandler.text;
+
+                var result = txt.Trim(new[] { '[', ']' });
+
+                if (int.TryParse(result, out int selection))
+                {
+                    return selection;
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                Debug.Log("Opponent GetRandomSelection timed out");
+            }
+
+            return -1;
         }
     }
 }
